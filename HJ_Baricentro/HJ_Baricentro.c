546b338b4d0a591e370aaae63f9befcd51a28c6f
Mateo_Jurado <hernan.jurado@udla.edu.ec>
2023-04-14 20:27:27 -0500

    ya

---
+++ HJ_Baricentro/HJ_Baricentro.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct {
+    float x;
+    float y;
+} Punto;
+
+int main() {
+    Punto A, B, C, baricentro;
+    float denominador;
+    
+    // Ingreso de los puntos A, B y C
+    printf("Ingrese las coordenadas del punto A:\n");
+    printf("x: ");
+    scanf("%f", &A.x);
+    printf("y: ");
+    scanf("%f", &A.y);
+    
+    printf("Ingrese las coordenadas del punto B:\n");
+    printf("x: ");
+    scanf("%f", &B.x);
+    printf("y: ");
+    scanf("%f", &B.y);
+    
+    printf("Ingrese las coordenadas del punto C:\n");
+    printf("x: ");
+    scanf("%f", &C.x);
+    printf("y: ");
+    scanf("%f", &C.y);
+    
+    // Cálculo del baricentro
+    denominador = (A.x * (B.y - C.y) + B.x * (C.y - A.y) + C.x * (A.y - B.y));
+    if (denominador == 0) {
+        printf("Los puntos ingresados no forman un triangulo.\n");
+        return 0;
+    }
+    
+    baricentro.x = (A.x + B.x + C.x) / 3;
+    baricentro.y = (A.y + B.y + C.y) / 3;
+    
+    // Verificación si el baricentro está dentro del triángulo
+    float areaABC = fabs((A.x*(B.y-C.y)+B.x*(C.y-A.y)+C.x*(A.y-B.y))/2.0);
+    float areaABP = fabs((A.x*(B.y-baricentro.y)+B.x*(baricentro.y-A.y)+baricentro.x*(A.y-B.y))/2.0);
+    float areaACP = fabs((A.x*(baricentro.y-C.y)+baricentro.x*(C.y-A.y)+C.x*(A.y-baricentro.y))/2.0);
+    float areaBCP = fabs((B.x*(C.y-baricentro.y)+baricentro.x*(B.y-C.y)+C.x*(baricentro.y-B.y))/2.0);
+    
+    if (areaABC == (areaABP + areaACP + areaBCP)) {
+        printf("El baricentro se encuentra dentro del triangulo.\n");
+    } else {
+        printf("El baricentro se encuentra fuera del triangulo.\n");
+    }
+    
+    printf("Coordenadas del baricentro: (%.2f, %.2f)\n", baricentro.x, baricentro.y);
+    
+    return 0;
+}
