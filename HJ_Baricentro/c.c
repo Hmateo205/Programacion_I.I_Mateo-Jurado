acd866bc47ca4bc3ea0d62c1155b03f97ddbf87d
Mateo_Jurado <hernan.jurado@udla.edu.ec>
2023-06-03 20:09:07 -0500

    as

---
+++ HJ_Baricentro/c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Ingrese su numero de cedula: \nIngrese sus apellidos: \nIngrese su nombre: \nQue especialidad desea:1.-a, 2.-b, 3.-c \na(10-11) , b(11-12) , c(12-13) \nEn que horario desea:  \n");
+    
+    printf("Ver las citas agendadas: Cita_1, Cita_2, Cita_3, Cita_4, Cita_4 \nCita1: Apellido,Nombre\n       Horario\n       Especialidad");
+
+    return 0;
+
+}
